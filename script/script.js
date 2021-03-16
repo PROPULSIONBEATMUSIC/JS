9d7bb10323f83282dbc595572bf12a378432bb6d
Kirill <narushev.k@mail.ru>
2021-03-16 15:29:16 +0300

    add trigger

--- script/script.js
+++ script/script.js
@@ -2,30 +2,54 @@ let todo_list = [];
 const input_elem = document.getElementById('input');
 const btn_elem = document.getElementById('btn');
 const output_elem = document.getElementById('output');
+const form_elem = document.getElementById('form_data');
+
 
 const up_elem = document.getElementById('sort_up');
 const down_elem = document.getElementById('sort_down');
+
 
 function render(){
     output_elem.innerText = '';
     todo_list.forEach(elem=>{
         const li_elem = document.createElement('li');
+        li_elem.classList.add('elem');
         li_elem.innerText = elem;
         output_elem.appendChild(li_elem);
+        li_elem.addEventListener('click', ()=>{
+            const elems = Document.getElementsByClassName('elem');
+            li_elems.classList.add('active');
+            [...elems].forEach(item=>item.classList.remove('active'));
+            li_elem.classList.add('active');
+        })
     })
 }
+
+render()
 
 up_elem.addEventListener('click', ()=>{
     todo_list = todo_list.sort();
     render();
-})
+    })
+
 
 down_elem.addEventListener('click', ()=>{
     todo_list = todo_list.sort().reverse();
     render();
 })
 
-btn_elem.addEventListener('click', ()=>{
+
+form_elem.addEventListener('submit', event=>{
+    event.preventDefault();
+    const value = input_elem.value;
+    input_elem.value = '';
+    todo_list.push(value);
+    render();
+})
+
+
+form_elem.addEventListener('submit', event=> {
+    event.preventDefault();
     const value = input_elem.value;
     input_elem.value = '';
     todo_list.push(value);
